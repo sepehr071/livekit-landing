0bf3cc06cf30521ebbd86e2d316ada0e460aa15a
alibasavad <alibasavad1381@gmail.com>
2025-07-20 13:00:55 +0330

    change support button

--- project/src/components/SupportButton.jsx
+++ project/src/components/SupportButton.jsx
@@ -23,11 +23,11 @@ const SupportButton = () => {
   
   return (
     <button
-      onClick={handleClick}
-      style={ isHidden ?  {transform: 'translateX(6rem)'}  : {}}
-      className={`fixed bottom-6 right-6 w-16 h-16 bg-orange-400 hover:bg-orange-500 hover:scale-110 text-white rounded-full shadow-lg flex items-center justify-center transition-all duration-[400ms] z-50 `}>
-      <Headphones size={30} />
-    </button>
+  onClick={handleClick}
+  style={isHidden ? { transform: 'translateX(6rem)' } : {}}
+  className={`animated-border fixed bottom-6 right-6 w-16 h-16 bg-orange-400 hover:bg-orange-500 hover:scale-110 text-white rounded-full shadow-lg flex items-center justify-center transition-all duration-[400ms] z-50`}>
+  <Headphones size={30} />
+</button>
   );
 };
 
